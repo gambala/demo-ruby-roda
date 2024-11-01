0a89474b3eeb971aee841727f9dce05dc0f37827
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-11-01 01:58:35 +0100

    Populate DB outside the class

--- app.rb
+++ app.rb
@@ -2,20 +2,20 @@ require 'roda'
 require 'json'
 require "sequel"
 
+DB = Sequel.sqlite
+DB.create_table :items do
+  primary_key :id
+  String :name, unique: true, null: false
+  Float :price, null: false
+end
+items = DB[:items]
+items.insert(name: 'abc', price: rand * 100)
+items.insert(name: 'def', price: rand * 100)
+items.insert(name: 'ghi', price: rand * 100)
+
 class App < Roda
   plugin :common_logger, $stdout
   plugin :render
-
-  DB = Sequel.sqlite
-  DB.create_table :items do
-    primary_key :id
-    String :name, unique: true, null: false
-    Float :price, null: false
-  end
-  items = DB[:items]
-  items.insert(name: 'abc', price: rand * 100)
-  items.insert(name: 'def', price: rand * 100)
-  items.insert(name: 'ghi', price: rand * 100)
 
   route do |r|
     r.root do
@@ -33,6 +33,7 @@ class App < Roda
     end
 
     r.get 'items' do
+      items = DB[:items]
       view 'items', locals: {items: items.order(Sequel.function(:RANDOM)).all, count: items.count, avg_price: items.avg(:price)}
     end
   end
