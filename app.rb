90947765e7af2f79fa639c3580ba1a967e0bc268
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-11-01 01:32:23 +0100

    Add html endpoint

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'json'
 
 class App < Roda
   plugin :common_logger, $stdout
+  plugin :render
 
   route do |r|
     r.root do
@@ -12,6 +13,11 @@ class App < Roda
     r.get 'json' do
       response['Content-Type'] = 'application/json'
       { message: 'Hello, world!' }.to_json
+    end
+
+    r.get 'html' do
+      messages = ["Good Morning", "Good Evening", "Good Night"]
+      view 'index', locals: {messages:}
     end
   end
 end
